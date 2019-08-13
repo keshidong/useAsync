12604c7444938a2faae4369682ef7f13e6122ed8
keshidong <keshidong.whu@gmail.com>
2019-08-14 00:41:13 +0800

    mod useAsync

--- src/useAsync.js
+++ src/useAsync.js
@@ -2,21 +2,79 @@ import { useState, useRef } from 'react'
 
 const useThat = (defaultThat) => useRef(defaultThat).current
 
-const useAsyncState = (initState) => {
+const useAsyncState = () => {
+    const [result, setResult] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+
+    return {
+        result,
+        setResult,
+        loading,
+        setLoading,
+        error,
+        setError,
+    }
+}
+
+const useCurrentPromise = () => {
+    const currentRef = useRef(null)
+    return {
+        set: (p) => { currentRef.current = p },
+        get: () => (currentRef.current),
+        is: (p) => (p === currentRef.current),
+    }
 
 }
 
+// promise with status and cancel function
 const useAsync = (asyncCallback) => {
-    const [loading, setLoading] = useState(false)
-    const pList = useThat([])
-    const returnRef = useThat({})
+    const returnThat = useThat({})
+    const {
+        result,
+        setResult,
+        loading,
+        setLoading,
+        error,
+        setError,
+    } = useAsyncState()
 
-    returnRef.execute = () => {
+    const {
+        is: isCurrentPromise,
+        set: setCurrentPromise,
+        get: getCurrentPromise,
+    } = useCurrentPromise()
+
+
+    returnThat.execute = () => {
+        const p = getCurrentPromise()
+        )
+        if (isCurrentPromise(p)) {
+            return
+        }
+
+        // 避免重复注册p的then callback
+        setCurrentPromise(p)
         setLoading(true)
-        pList.push(asyncCallback())
+
+        Promise.resolve(p)
+            .then((result) => {
+                setResult(result)
+            })
+            .catch((e) => {
+                setError(e)
+            })
+            .then(() => {
+                setLoading(false)
+            })
+
+
+
     }
 
-    returnRef.loading = loading
+    returnThat.loading = loading
+    returnThat.error = error
+    returnThat.result = result
 
-    return returnRef
+    return returnThat
 }
